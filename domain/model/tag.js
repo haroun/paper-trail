05edf9a334cb482f6837f54ddd466a3efeab5786
Harouna Traore <h.traore@icloud.com>
2017-11-10 23:53:20 +0100

    list available tags

---
+++ domain/model/tag.js
@@ -0,0 +1,26 @@
+const tag = [
+  'salary',
+  'bank',
+  'insurance',
+  'electricity',
+  'water',
+  'rent',
+  'cloth',
+  'transport',
+  'health',
+  'car',
+  'food',
+  'withdrawal',
+  'outing',
+  'leisure',
+  'vacation',
+  'transfer',
+  'saving',
+  'caf',
+  'check',
+  'tax',
+  'nursery',
+  'subscription'
+]
+
+module.exports.tag = tag

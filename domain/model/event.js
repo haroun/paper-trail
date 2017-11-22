43a5a2a8f3be398419856dc04a322645c24c82fc
Harouna Traore <h.traore@icloud.com>
2017-11-22 23:09:10 +0100

    update event mixin

--- domain/model/event.js
+++ domain/model/event.js
@@ -1,9 +1,36 @@
-const event = {
-  id: null,
-  version: null,
-  name: null,
-  at: null,
-  attributes: null
+const eventMixin = ({
+  version = null,
+  occuredAt = null,
+  accountNumber = null,
+  type = null
+}) => {
+  if (version === null) {
+    throw new TypeError('version cannot be null')
+  }
+
+  if (occuredAt === null) {
+    throw new TypeError('occuredAt cannot be null')
+  }
+
+  if (accountNumber === null) {
+    throw new TypeError('accountNumber cannot be null')
+  }
+
+  if (type === null) {
+    throw new TypeError('type cannot be null')
+  }
+
+  const id = null
+  const noticedAt = null
+
+  return {
+    id: () => id,
+    version: () => version,
+    occuredAt: () => occuredAt,
+    noticedAt: () => noticedAt,
+    accountNumber: () => accountNumber,
+    type: () => type
+  }
 }
 
-module.exports.event = event
+module.exports.event = eventMixin

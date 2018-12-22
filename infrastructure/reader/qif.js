8cb841ec74266cbca3c97ea96ac35e2a864bb22b
Harouna Traore <h.traore@icloud.com>
2018-12-22 15:27:01 +0100

    qif reader must return date as number

--- infrastructure/reader/qif.js
+++ infrastructure/reader/qif.js
@@ -1,24 +1,41 @@
 const fs = require('fs')
 
-// TODO: transform amount in cents
-
 const NEWLINE_REGEX = /\r?\n|\u2028|\u2029/
 
+// Load a file
+// load :: (String) -> String
 const load = file => fs.readFileSync(file)
+
+// Split
+// split :: (String, String) -> [String]
 const split = (content, separator) => content.toString().split(separator)
+
+// Convert to cents
+// toCents :: String -> Number
+// toCents :: Number -> Number
 const toCents = amount => parseFloat(amount, 10) * 100
+
+// Read file
+// read :: (Object, Object) -> Object
 const read = (item, mapper) => item
   .filter(line => Object.keys(mapper).indexOf(line.charAt(0)) !== -1)
   .reduce((acc, line) => {
     const code = line.charAt(0)
-    const value = mapper[code] === 'amount' ? toCents(line.slice(1)) : line.slice(1)
+    const value = mapper[code] === 'amount'
+      ? toCents(line.slice(1))
+      : mapper[code] === 'date'
+      ? Date.parse(line.slice(1))
+      : line.slice(1)
     const entry = {
       [mapper[code]]: value
     }
 
-    return Object.assign({}, acc, entry)
+    return {...acc, ...entry}
   }, {})
 
+// Parse
+// parse :: (String, {D: String, T: String, P: String}, String) -> [{date: Number, amount: Number, description: String}]
+// parse :: (String, Object, String) -> [Object]
 const parse = (file, mapper = {D: 'date', T: 'amount', P: 'description'}, separator = '^') => {
   let content
 
--- infrastructure/reader/qif.test.js
+++ infrastructure/reader/qif.test.js
@@ -7,22 +7,22 @@ test('parse qif file', assert => {
   const actual = qif.parse('./fixtures/input.qif')
   const expected = [
     {
-      date: '31/12/2017',
+      date: 1514674800000,
       amount: -4760,
-      description: 'CARTE 25/11/2017 UNIQLO LYON CAR'
+      description: 'CARTE 25/11/2017 XXXX XXXX XXXX'
     },
     {
-      date: '06/12/2017',
+      date: 1512514800000,
       amount: -5000,
-      description: 'RETRAIT DAB 03/12/2017 LYON GROS CAILLOU'
+      description: 'RETRAIT DAB 03/12/2017 XXXX XXXX XXXX'
     },
     {
-      date: '05/12/2017',
+      date: 1512428400000,
       amount: -1950,
       description: 'PRLV SEPA XXXX XXXX : XXXX XXXX : XXXX XXXX'
     },
     {
-      date: '01/12/2017',
+      date: 1512082800000,
       amount: 317549,
       description: 'VIREMENT SEPA RECU XXXX XXXX XXXX'
     }
